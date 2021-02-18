b8fa4c6d2adb7e40a843149fd92fc29cba40ef9b
jmnguyen1999 <jmnguyen1999@gmail.com>
2021-02-18 14:20:14 -0800

    Making the Details page scrollable with ScrollView, and creating a MovieDBClient class to make network requests separately

--- app/src/main/java/com/example/flixster/activities/FlixsterActivity.java
+++ app/src/main/java/com/example/flixster/activities/FlixsterActivity.java
@@ -22,6 +22,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.R;
 import com.example.flixster.adapters.MovieAdapter;
 import com.example.flixster.models.Movie;
+import com.example.flixster.network.MovieDBClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -36,9 +37,12 @@ public class FlixsterActivity extends AppCompatActivity {
     //Constants/Fields:
     public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
     public static final String CONFIGURATION_URL = "https://api.themoviedb.org/3/configuration?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
-    private static final String TAG = "MainActivity";        //For Log.d statements
+    private static final String TAG = "FlixsterActivity";        //For Log.d statements
 
     List<Movie> movies;
+    MovieDBClient client;
+    RecyclerView rvMovies;
+    MovieAdapter movieAdapter;
     String movieBackdropSize;
     String moviePosterSize;
 
@@ -54,20 +58,27 @@ public class FlixsterActivity extends AppCompatActivity {
         movies = new ArrayList<>();
 
         //1.) Set up RecyclerView: Create and bind MovieAdapter and LayoutManager
-        RecyclerView rvMovies = findViewById(R.id.rvMovies);
-        MovieAdapter movieAdapter = new MovieAdapter(this, movies);         //note: movies[] will be updated later
+        rvMovies = findViewById(R.id.rvMovies);
+        movieAdapter = new MovieAdapter(this, movies);         //note: movies[] will be updated later
         rvMovies.setAdapter(movieAdapter);
         rvMovies.setLayoutManager(new LinearLayoutManager(this));
 
-        //2.)Create client and execute network requests to API
-        AsyncHttpClient client = new AsyncHttpClient();
+        //2.) Obtain now playing movies and update movieAdapter with movies List
+        fetchMovies();
+        //2.)Create MovieDBClient to: (1) Get and choose movie images sizes, (2) Get now-playing movies, (3) Create a list of Movie objects with found info, and (4)Notifies movieAdapter of updated model
 
-        //2a.) Get available images sizes and choose the median size as each movie's "posterPathSize" and "backdropPathSize":
-        client.get(CONFIGURATION_URL, new JsonHttpResponseHandler() {
+    }
+
+    /**
+     * Purpose:     Makes configuration network request via MovieDBClient class to initialize fields: : "posterPathSize" and "backdropPathSize". Gets available movie image sizes and chooses the median size for the intialization.
+     */
+    private void fetchConfiguration(){
+        client = new MovieDBClient();
+        client.makeConfigurationRequest(new JsonHttpResponseHandler() {
             @Override
             //Initializes "movieBackdropSize" and "moviePosterSize" on success response:
             public void onSuccess(int i, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess(): get(CONFIGURATION_URL)");
+                Log.d(TAG, "getMovies(): onSuccess()-get(CONFIGURATION_URL)");
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONObject images = jsonObject.getJSONObject("images");
@@ -77,42 +88,51 @@ public class FlixsterActivity extends AppCompatActivity {
                     movieBackdropSize = backdropSizes.get(backdropSizes.length()/2).toString();     //all sizes are sorted --> choose middle index
                     moviePosterSize = posterSizes.get(posterSizes.length()/2).toString();
                 } catch (JSONException e) {
-                    Log.e(TAG, "Hit JSON exception - onSuccess(): get(CONFIGURATION_URL)", e);
+                    Log.e(TAG, "getMovies(): Hit JSON exception - onSuccess(): get(CONFIGURATION_URL)", e);
                     e.printStackTrace();
                 }
             }
-
             @Override
             public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-                Log.d(TAG, "onFailure(): get(CONFIGURATION_URL)");
+                Log.d(TAG, "getMovies(): onFailure() - get(CONFIGURATION_URL)");
             }
         });
+    }
 
-        //2b.) Get and save now playing movies into "movies" List
-        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+
+    /**
+     * Purpose:         Makes network requst via MovieDBClient to get Now Playing movies. Creates list of Movies objects with image sizes chosen from fetchConfiguration(), and updates RecyclerView "movieAdpater" field
+     */
+    private void fetchMovies(){
+        //1.) Get available movie image sizes and choose the "posterPathSize" and "backdropPathSize" to use when initializing Movie objects later
+        fetchConfiguration();
+
+        //2.) Make network request to get now playing movies, initialize "movies" List, and update adapter:
+        client = new MovieDBClient();
+        client.makeNowPlayingRequest(new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int i, Headers headers, JSON json) {
-                Log.d(TAG, "onSuccess(): get(NOW_PLAYING_URL");
+                Log.d(TAG, "fetchMovies(): onSuccess()");
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results.toString());
 
-                    movies.addAll(Movie.jsonArrayToList(results, moviePosterSize, movieBackdropSize));      //initializes every movie to same poster and backdrop size
+                    movies.addAll(Movie.jsonArrayToList(results, moviePosterSize, movieBackdropSize));//initializes every movie to same poster and backdrop size
+                    // Log.i(TAG, "Movies List: " + movies.toString());
+                    Log.i(TAG, "Movies: " + movies.size());
                     movieAdapter.notifyDataSetChanged();
 
-                    Log.i(TAG, "Movies: " + movies.size());
                 } catch (JSONException e) {
-                    Log.e(TAG, "Hit JSON exception - onSuccess(): get(NOW_PLAYING_URL)", e);
+                    Log.e(TAG, "fetchMovies(): Hit JSON exception - onSuccess()", e);
                     e.printStackTrace();
                 }
             }
 
             @Override
             public void onFailure(int i, Headers headers, String s, Throwable throwable) {
-                Log.d(TAG, "onFailure() : get(NOW_PLAYING_URL)");
+                Log.d(TAG, "fetchMovies(): onFailure()");
             }
         });
-
     }
 }
---
+++ app/src/main/java/com/example/flixster/network/MovieDBClient.java
@@ -0,0 +1,40 @@
+package com.example.flixster.network;
+
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixster.adapters.MovieAdapter;
+import com.example.flixster.models.Movie;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class MovieDBClient {
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String CONFIGURATION_URL = "https://api.themoviedb.org/3/configuration?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    private static final String YOUTUBE_API_KEY = "AIzaSyD0CkaPZjI5CTA5eAJik0ieuXxwzbr1MTA";
+    public static final String VIDEO_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MovieDBClient";
+
+    private AsyncHttpClient client;
+
+    public MovieDBClient(){
+        client = new AsyncHttpClient();
+    }
+
+    public void makeConfigurationRequest(JsonHttpResponseHandler handler){
+        client.get(CONFIGURATION_URL, handler);
+    }
+
+    public void makeNowPlayingRequest(JsonHttpResponseHandler handler){
+        client.get(NOW_PLAYING_URL, handler);
+    }
+}
